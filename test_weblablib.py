eb17474f0c9eee0b01a72f08256d390ea7c73367
Pablo Orduña <pablo.orduna@deusto.es>
2017-08-20 23:24:51 -0700

    Start adding tests

--- test_weblablib.py
+++ test_weblablib.py
@@ -1,4 +1,22 @@
+from flask import Flask
 import weblablib
+import unittest
 
-def test_check():
-    assert 1 == 1
+class WebLabTest(unittest.TestCase):
+
+    def create_weblab(self):
+        self.weblab = weblablib.WebLab()
+        self.app = Flask(__name__)
+        self.app.config.update({
+            'WEBLAB_CALLBACK_URL': '/mylab/callback',
+            'WEBLAB_USERNAME': 'weblabdeusto',
+            'WEBLAB_PASSWORD': 'password',
+        })
+        self.weblab.init_app(self.app)
+
+    def setUp(self):
+        self.create_weblab()
+
+    def test_weblab(self):
+        pass
+
