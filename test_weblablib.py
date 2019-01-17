23ea889889de2ba9da26a5abd9c6b25493a5aceb
Pablo Orduña <pablo@labsland.com>
2019-01-16 17:12:25 -0800

    Fix test

--- test_weblablib.py
+++ test_weblablib.py
@@ -421,7 +421,7 @@ class UserTest(BaseSessionWebLabTest):
             return "ERROR"
         weblablib.current_task.data = {'inside': 'previous'}
         weblablib.current_task.update_data({'inside': 'yes'})
-        weblablib.weblab_user.data.download()
+        weblablib.weblab_user.data.retrieve()
         return [ self.counter, weblablib.weblab_user.data['foo'] ]
 
     def test_simple(self):
